3c152c7187fdf90e3561470ad2c22adae5dd03da
Ali Shah <s.ashah@outlook.com>
2021-07-03 03:01:26 -0400

    [Added] functionality for downloading gopher
    Notes:
    - Found a reference for how to download images in the chromedp
      examples repository (github.com/chromedp/examples)

--- main/go.mod
+++ main/go.mod
@@ -2,4 +2,7 @@ module github.com/shah-a/bew2.5-02-makescraper
 
 go 1.16
 
-require github.com/chromedp/chromedp v0.7.3
+require (
+	github.com/chromedp/cdproto v0.0.0-20210526005521-9e51b9051fd0
+	github.com/chromedp/chromedp v0.7.3
+)
--- main/main.go
+++ main/main.go
@@ -4,15 +4,13 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
+	"io/ioutil"
+	"path/filepath"
 
+	"github.com/chromedp/cdproto/network"
 	"github.com/chromedp/chromedp"
 )
-
-/*
- * TODO:
- * - Figure out how to grab the download URL from <img class=".big-gopher"> ✅
- * - Figure out how to actually save the image to current directory ❌
- */
 
 func main() {
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
@@ -29,15 +27,18 @@ func main() {
 	// ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 
-	var downloadURL string
+	// create a timeout as a safety net to prevent any infinite wait loops
+	ctx, cancel = context.WithTimeout(ctx, 60*time.Second)
+	defer cancel()
+
+	var downloadUrl string
 	var ok bool
 
 	if err := chromedp.Run(ctx,
 		chromedp.Navigate("https://gopherize.me/"),
 		chromedp.Click("#shuffle-button", chromedp.ByQuery),
 		chromedp.Click("#next-button", chromedp.ByQuery),
-		chromedp.AttributeValue(".big-gopher", "src", &downloadURL, &ok, chromedp.ByQuery),
-		// chromedp.WaitNotPresent(".big-gopher", chromedp.ByQuery),
+		chromedp.AttributeValue(".big-gopher", "src", &downloadUrl, &ok, chromedp.ByQuery),
 	); err != nil {
 		log.Fatal(err)
 	}
@@ -46,5 +47,53 @@ func main() {
 		log.Fatal("Could not scrape img src")
 	}
 
-	fmt.Printf("Download URL: %s\n", downloadURL)
+	// set up a channel so we can block later while we monitor the download progress
+	downloadComplete := make(chan bool)
+
+	// this will be used to capture the request id for matching network events
+	var requestId network.RequestID
+
+	// set up a listener to watch the network events and close the channel when complete
+	// the request id matching is important both to filter out unwanted network events
+	// and to reference the downloaded file later
+	chromedp.ListenTarget(ctx, func(v interface{}) {
+		switch ev := v.(type) {
+		case *network.EventRequestWillBeSent:
+			// fmt.Printf("EventRequestWillBeSent: %v: %v\n", ev.RequestID, ev.Request.URL)
+			if ev.Request.URL == downloadUrl {
+				requestId = ev.RequestID
+			}
+		case *network.EventLoadingFinished:
+			// fmt.Printf("EventLoadingFinished: %v\n", ev.RequestID)
+			if ev.RequestID == requestId {
+				close(downloadComplete)
+			}
+		}
+	})
+
+	if err := chromedp.Run(ctx,
+		chromedp.Navigate(downloadUrl),
+	); err != nil {
+		log.Fatal(err)
+	}
+
+	// this will block until the chromedp listener closes the channel
+	<-downloadComplete
+
+	var downloadBytes []byte
+	if err := chromedp.Run(ctx, chromedp.ActionFunc(func(ctx context.Context) error {
+		var err error
+		downloadBytes, err = network.GetResponseBody(requestId).Do(ctx)
+		return err
+	})); err != nil {
+		log.Fatal(err)
+	}
+
+	// write the file to disk - since we hold the bytes we dictate the name and location
+	downloadDest := filepath.Join(".", "my-gopher.png")
+	if err := ioutil.WriteFile(downloadDest, downloadBytes, 0644); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Download Complete! Your gopher was saved as `%s`", downloadDest)
 }
