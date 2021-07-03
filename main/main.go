8877ea1470cb52b03a7f1949a290cf2cf2e805ca
Ali Shah <s.ashah@outlook.com>
2021-07-03 01:07:00 -0400

    [Added] TODO comment

--- main/main.go
+++ main/main.go
@@ -7,6 +7,12 @@ import (
 	"github.com/chromedp/chromedp"
 )
 
+/*
+ * TODO:
+ * - Figure out how to grab the download URL from <img class=".big-gopher">
+ * - Figure out how to actually save the image to current directory
+ */
+
 func main() {
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
 		chromedp.Flag("headless", false),
@@ -20,6 +26,8 @@ func main() {
 
 	ctx, cancel := chromedp.NewContext(allocCtx)
 	defer cancel()
+
+	downloadURL := "https://storage.googleapis.com/gopherizeme.appspot.com/gophers/3fbbd6ea507241a0e663656182ac58d2b811b5f1.png"
 
 	if err := chromedp.Run(ctx,
 		chromedp.Navigate("https://gopherize.me/"),
